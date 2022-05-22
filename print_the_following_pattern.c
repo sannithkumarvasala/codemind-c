ba3df99b2b0bde5cd655449fc3ff9f135a55620e
SANNITH KUMAR VASALA <93171155+sannithkumarvasala@users.noreply.github.com>
2022-05-22 14:09:17 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&i);     n=i;     for(i;i>=1;i--)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,j;
+    int i,j,n;
     scanf("%d",&i);
+    n=i;
     for(i;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>=1;j--)
         {
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("
 ");
