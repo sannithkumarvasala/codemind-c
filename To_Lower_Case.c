25b6a728ca91070e1935ad31122237e525ca63ca
SANNITH KUMAR VASALA <93171155+sannithkumarvasala@users.noreply.github.com>
2022-05-29 12:19:14 +0530

    #include<stdio.h> int main() {     int i;     char s[1000];     scanf("%s",s);     for(i=0;s[i]!=0;i++)     {         if(s[i]>='A' && s[i]<='Z')         {             s[i]=s[i]+32;         }     }     printf("%s",s); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char s[1000];
+    scanf("%s",s);
+    for(i=0;s[i]!=0;i++)
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            s[i]=s[i]+32;
+        }
+    }
+    printf("%s",s);
+}
